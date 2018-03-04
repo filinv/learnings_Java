e401a181e2c69621eb82a1f4c32f9be029942314
Vladimir <decdom@mail.ru>
2018-03-04 18:44:34 +0300

    Update Arkanoid.java

--- rush/projects/Arcanoid/Arkanoid.java
+++ rush/projects/Arcanoid/Arkanoid.java
@@ -1,3 +1,17 @@
 public class Arkanoid {
+    private int width, height;
     public static void main(String[] args){}
+
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
 }
