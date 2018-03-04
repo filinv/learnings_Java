2b5623cd77f2cc96ff22826afa6190863e0ecc40
Vladimir <decdom@mail.ru>
2018-03-04 21:24:39 +0300

    Update Ball.java
    overrided two void methods draw(Canvas), move()

--- rush/projects/Arcanoid/Ball.java
+++ rush/projects/Arcanoid/Ball.java
@@ -2,4 +2,10 @@ public class Ball extends BaseObject{
   public Ball(double x, double y, double radius) {
         super(x, y, radius);
     }
+  @Override
+    public void draw(Canvas canvas) {
+    }
+  @Override
+    public void move() {
+    }
 }
