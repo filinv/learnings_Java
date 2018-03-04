4fc077a88dca8a9ee46f8895d28bfa8434f25bc2
Vladimir <decdom@mail.ru>
2018-03-04 21:22:11 +0300

    Update BaseObject.java
    does class abstract, added two abstract void methods draw(Canvas),move() and boolean method isIntersec(BaseObject)

--- rush/projects/Arcanoid/BaseObject.java
+++ rush/projects/Arcanoid/BaseObject.java
@@ -1,10 +1,16 @@
-public class BaseObject {
-  private double x,y,radius;
+public abstract class BaseObject {
+    private double x,y,radius;
 
     public BaseObject(double x, double y, double radius) {
         this.x = x;
         this.y = y;
         this.radius = radius;
+    }
+    public abstract void draw(Canvas canvas);
+    public abstract void move();
+    boolean isIntersec(BaseObject o){
+        double distance=Math.sqrt(Math.pow(this.x-o.x,2)+Math.pow(this.y-o.y,2));
+        return distance<=Math.max(this.radius,o.radius);
     }
     public double getX() {
         return x;
