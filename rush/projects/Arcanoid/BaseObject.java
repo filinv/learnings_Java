a70264d6270d25bdd461aa14973a22609acf1249
Vladimir <decdom@mail.ru>
2018-03-04 19:25:39 +0300

    Update BaseObject.java
    added:
    1.private fields double x,y,radius
    2.public constructor(double,double,double)
    3. it's public getters and setters

--- rush/projects/Arcanoid/BaseObject.java
+++ rush/projects/Arcanoid/BaseObject.java
@@ -1,2 +1,27 @@
 public class BaseObject {
+  private double x,y,radius;
+
+    public BaseObject(double x, double y, double radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    public double getX() {
+        return x;
+    }
+    public double getY() {
+        return y;
+    }
+    public double getRadius() {
+        return radius;
+    }
+    public void setX(double x) {
+        this.x = x;
+    }
+    public void setY(double y) {
+        this.y = y;
+    }
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
 }
