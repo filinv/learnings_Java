9b7ecf16e6eaf4a29c62d0e70a694657ae1ef9bf
Vladimir <decdom@mail.ru>
2018-03-04 22:19:42 +0300

    Update Brick.java
    edited constructor

--- rush/projects/Arcanoid/Brick.java
+++ rush/projects/Arcanoid/Brick.java
@@ -1,6 +1,6 @@
 public class Brick extends BaseObject{
-  public Brick(double x, double y, double radius) {
-        super(x, y, radius);
+  public Brick(double x, double y) {
+        super(x, y, 3);
     }
   @Override
     public void draw(Canvas canvas) {
