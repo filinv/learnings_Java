2216e52350dd9d66f0ddb34c93de81566d611d9b
Vladimir <decdom@mail.ru>
2018-03-04 21:33:56 +0300

    Update Canvas.java
    added:
    1.fields private int width,height, private char[][] matrix;
    2.public constructor with matrix initialization
    3.it's getters and setters

--- rush/projects/Arcanoid/Canvas.java
+++ rush/projects/Arcanoid/Canvas.java
@@ -1,2 +1,28 @@
 public class Canvas {
+  private int width,height;
+    private char[][] matrix;
+
+    public Canvas(int width, int height) {
+        this.width = width;
+        this.height = height;
+        matrix=new char[height+2][width+2];
+    }
+    public int getWidth() {
+        return width;
+    }
+    public int getHeight() {
+        return height;
+    }
+    public char[][] getMatrix() {
+        return matrix;
+    }
+    public void setWidth(int width) {
+        this.width = width;
+    }
+    public void setHeight(int height) {
+        this.height = height;
+    }
+    public void setMatrix(char[][] matrix) {
+        this.matrix = matrix;
+    }
 }
