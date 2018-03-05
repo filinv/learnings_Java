f8e793d864c18e2970e62e831b5d0a1643a4d3c1
Vladimir <decdom@mail.ru>
2018-03-05 22:30:54 +0300

    Update Brick.java
    added private static int[][] matrix;
    edited draw(Canvas);

--- rush/projects/Arcanoid/Brick.java
+++ rush/projects/Arcanoid/Brick.java
@@ -1,9 +1,18 @@
 public class Brick extends BaseObject{
+  //картинка для отрисовки
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 1, 1, 1, 0},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
   public Brick(double x, double y) {
         super(x, y, 3);
     }
   @Override
     public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'H');
     }
     @Override
     public void move() {
