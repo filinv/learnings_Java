8d24e0ec8d72c4224fb644cef4a86dfc146f8b20
Vladimir <decdom@mail.ru>
2018-03-05 22:18:53 +0300

    Update Stand.java
    added private static int[][] matrix;
    initialized fields speed and direction beyond constructor;
    edited methods move() and draw(Canvas);

--- rush/projects/Arcanoid/Stand.java
+++ rush/projects/Arcanoid/Stand.java
@@ -1,16 +1,27 @@
 public class Stand extends BaseObject{
-    private double speed, direction;
+    //картинка для отрисовки
+    private static int[][] matrix = {
+            {1, 1, 1, 1, 1},
+            {1, 0, 0, 0, 1},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0},
+    };
+    private double speed = 1;
+    //направление (-1 влево, +1 вправо)
+    private double direction = 0;
     public Stand(double x, double y) {
-        super(x, y, 3);
-        speed = 1;
-        direction = 0;
+        super(x, y, 3);        
     }
     @Override
     public void draw(Canvas canvas) {
+        canvas.drawMatrix(x - radius + 1, y, matrix, 'M');
     }
     @Override
     public void move() {
-        x+=direction*speed;
+        double dx = speed * direction;
+        x = x + dx;
+        checkBorders(radius, Arkanoid.game.getWidth() - radius + 1, 1, Arkanoid.game.getHeight() + 1);
     }
     public double getSpeed() {
         return speed;
