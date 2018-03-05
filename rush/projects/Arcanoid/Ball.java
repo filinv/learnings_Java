c8f16ee9bbc8ced831369fd7d296aee43be8de74
Vladimir <decdom@mail.ru>
2018-03-05 21:03:27 +0300

    Update Ball.java
    added public void start();
    edited draw(Canvas) and move()

--- rush/projects/Arcanoid/Ball.java
+++ rush/projects/Arcanoid/Ball.java
@@ -10,9 +10,17 @@ public class Ball extends BaseObject{
     }
     @Override
     public void draw(Canvas canvas) {
+        canvas.setPoint(x,y,'O');
     }
     @Override
     public void move() {
+        if(!isFrozen){
+            x+=dx;
+            y+=dy;
+        }
+    }
+    public void start(){
+        isFrozen=false;
     }
     public double getSpeed() {
         return speed;
