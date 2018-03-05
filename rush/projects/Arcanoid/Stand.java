bd0bdb3d220b589afd23304e21291a1d21f33e0f
Vladimir <decdom@mail.ru>
2018-03-05 21:16:11 +0300

    Update Stand.java
    added:
    1. fields private double speed, direction;
    2. edited constructor
    3. getters for these fields

--- rush/projects/Arcanoid/Stand.java
+++ rush/projects/Arcanoid/Stand.java
@@ -1,11 +1,20 @@
 public class Stand extends BaseObject{
-  public Stand(double x, double y, double radius) {
-        super(x, y, radius);
+    private double speed, direction;
+    public Stand(double x, double y) {
+        super(x, y, 3);
+        speed = 1;
+        direction = 0;
     }
-  @Override
+    @Override
     public void draw(Canvas canvas) {
     }
     @Override
     public void move() {
+    }
+    public double getSpeed() {
+        return speed;
+    }
+    public double getDirection() {
+        return direction;
     }
 }
