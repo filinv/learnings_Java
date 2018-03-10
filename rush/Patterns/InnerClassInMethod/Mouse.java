eba1cf78f435dde2f2af601688ffcbbe8c5c94f3
Vladimir <decdom@mail.ru>
2018-03-10 20:49:33 +0300

    Create Mouse.java

---
+++ rush/Patterns/InnerClassInMethod/Mouse.java
@@ -0,0 +1,14 @@
+/*
+обратите внимание, как именно Mouse отличается от Dog
+Этот класс - привычный для вас.
+*/
+public class Mouse implements Pet, Sayable {
+    @Override
+    public Sayable toSayable(int i) {
+        return this;
+    }
+    @Override
+    public String say() {
+        return "Мышь пищит.";
+    }
+}
