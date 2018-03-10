042ca409adfdd1472e1035cf3234c28b2a2545a0
Vladimir <decdom@mail.ru>
2018-03-10 20:48:25 +0300

    Create SuperDog.java

---
+++ rush/Patterns/InnerClassInMethod/SuperDog.java
@@ -0,0 +1,9 @@
+import java.text.SimpleDateFormat;
+
+public abstract class SuperDog {
+    protected String getSuperQuotes() {
+        //some logic here
+        return " *** ";
+    }
+    protected SimpleDateFormat formatter = new SimpleDateFormat("dd-MMM-yyyy EEE");
+}
