8aa8caf143ffea57e1a9f0ef1247902855d2d5a3
Vladimir <decdom@mail.ru>
2018-03-11 17:40:21 +0300

    Create MainModel.java

---
+++ rush/Patterns/MVC/model/MainModel.java
@@ -0,0 +1,15 @@
+import java.util.List;
+
+public class MainModel implements Model {
+    private ModelData modelData=new ModelData();
+    private UserService userService=new UserServiceImpl();
+    @Override
+    public ModelData getModelData() {
+        return modelData;
+    }
+    @Override
+    public void loadUsers() {
+        List<User>list=userService.getUsersBetweenLevels(1,100);
+        modelData.setUsers(list);
+    }
+}
