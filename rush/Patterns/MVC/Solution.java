cfe99e9a5cdccbfc2925debf72dcea6b30c65854
Vladimir <decdom@mail.ru>
2018-03-11 19:03:01 +0300

    Update Solution.java
    added:
    1. EditUserView editUserView = new EditUserView();
    2. controller.setEditUserView(editUserView);
    3. usersView.fireEventOpenUserEditForm(126);

--- rush/Patterns/MVC/Solution.java
+++ rush/Patterns/MVC/Solution.java
@@ -2,13 +2,16 @@ public class Solution {
     public static void main(String[] args) {
         Model model = new MainModel();
         UsersView usersView = new UsersView();
+        EditUserView editUserView = new EditUserView();
         Controller controller = new Controller();
 
         usersView.setController(controller);
         controller.setModel(model);
         controller.setUsersView(usersView);
+        controller.setEditUserView(editUserView);
 
         usersView.fireEventShowAllUsers();
+        usersView.fireEventOpenUserEditForm(126);
         usersView.fireEventShowDeletedUsers();
     }
 }
