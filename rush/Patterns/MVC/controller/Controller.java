72c3d848e274195a29e8fa092b9638d80f1047b4
Vladimir <decdom@mail.ru>
2018-03-11 17:59:54 +0300

    Update Controller.java
    added void onShowAllDeletedUsers()

--- rush/Patterns/MVC/controller/Controller.java
+++ rush/Patterns/MVC/controller/Controller.java
@@ -9,6 +9,10 @@ public class Controller {
         model.loadUsers();
         usersView.refresh(model.getModelData());
     }
+    public void onShowAllDeletedUsers() {
+        model.loadDeletedUsers();
+        usersView.refresh(model.getModelData());
+    }
     public void setUsersView(UsersView usersView) {
         this.usersView = usersView;
     }
