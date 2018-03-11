7e1fa26383412249b272b4ad8f252f6f5e41b2d7
Vladimir <decdom@mail.ru>
2018-03-11 17:26:05 +0300

    Create UserService.java

---
+++ rush/Patterns/MVC/model/service/UserService.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public interface UserService {
+    public User deleteUser(long id);
+
+    public User createOrUpdateUser(String name, long id, int level);
+
+    public List<User> getUsersByName(String name);
+
+    public List<User> getAllDeletedUsers();
+
+    public List<User> getUsersBetweenLevels(int fromLevel, int toLevel);
+
+    List<User> filterOnlyActiveUsers(List<User> allUsers);
+
+    User getUsersById(long userId);
+}
