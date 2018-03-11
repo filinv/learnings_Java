5a00dfcd663c222c79385e8f2a8bde58d4fbb7f2
Vladimir <decdom@mail.ru>
2018-03-11 18:26:53 +0300

    Update UsersView.java
    edited method refresh: added condition for isDisplayDeletedUserList()

--- rush/Patterns/MVC/view/UsersView.java
+++ rush/Patterns/MVC/view/UsersView.java
@@ -8,7 +8,8 @@ public class UsersView implements View {
     }
     @Override
     public void refresh(ModelData modelData) {
-        System.out.println("All users:");
+        if(modelData.isDisplayDeletedUserList())System.out.println("All deleted users:");
+        else System.out.println("All users:");
         for(User user:modelData.getUsers()){
             System.out.println("\t"+user);
         }
