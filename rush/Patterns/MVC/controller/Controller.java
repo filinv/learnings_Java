4694891b968369972b95822378a07b545708a55d
Vladimir <decdom@mail.ru>
2018-03-11 17:21:37 +0300

    Update Controller.java
    added: 
    1.private UsersView usersView;
    2.usersView.refresh(model.getModelData()); to method onShowAllUsers()
    3.setter for usersView

--- rush/Patterns/MVC/controller/Controller.java
+++ rush/Patterns/MVC/controller/Controller.java
@@ -1,10 +1,15 @@
 public class Controller {
     private Model model;
+    private UsersView usersView;
 
     public void setModel(Model model) {
         this.model = model;
     }
     public void onShowAllUsers(){
         model.loadUsers();
+        usersView.refresh(model.getModelData());
+    }
+    public void setUsersView(UsersView usersView) {
+        this.usersView = usersView;
     }
 }
