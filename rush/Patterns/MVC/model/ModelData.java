4ce90a8dfd7da23c00fea983e7afb4dce857eca1
Vladimir <decdom@mail.ru>
2018-03-11 13:08:30 +0300

    Create ModelData.java

---
+++ rush/Patterns/MVC/model/ModelData.java
@@ -0,0 +1,12 @@
+import java.util.List;
+
+public class ModelData {
+    private List<User> users;
+
+    public List<User> getUsers() {
+        return users;
+    }
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
