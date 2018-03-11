85ceee8a138fc193636783227b4db0a22e3227cb
Vladimir <decdom@mail.ru>
2018-03-11 18:27:28 +0300

    Create EditUserView.java

---
+++ rush/Patterns/MVC/view/EditUserView.java
@@ -0,0 +1,13 @@
+public class EditUserView implements View {
+    private Controller controller;
+    @Override
+    public void refresh(ModelData modelData) {
+        System.out.println("User to be edited:");
+        System.out.println("\t"+modelData.getActiveUser());
+        System.out.println("===================================================");
+    }
+    @Override
+    public void setController(Controller controller) {
+        this.controller=controller;
+    }
+}
