77f8f7a43211970138b1a1ddd5c013dd3703b082
Vladimir <decdom@mail.ru>
2018-03-17 16:37:03 +0300

    Update EditUserView.java
    added public void fireEventUserChanged(String name, long id, int level)

--- rush/Patterns/MVC/view/EditUserView.java
+++ rush/Patterns/MVC/view/EditUserView.java
@@ -13,4 +13,7 @@ public class EditUserView implements View {
     public void fireEventUserDeleted(long id){
         controller.onUserDelete(id);
     }
+    public void fireEventUserChanged(String name, long id, int level){
+        controller.onUserChange(name,id,level);
+    }
 }
