10b1ed071f8f93dc936334a485de6c5fe1d8140b
Vladimir <decdom@mail.ru>
2018-03-17 15:55:57 +0300

    Update Controller.java
    added public void onUserDelete(long id)

--- rush/Patterns/MVC/controller/Controller.java
+++ rush/Patterns/MVC/controller/Controller.java
@@ -18,6 +18,10 @@ public class Controller {
         model.loadUserById(userId);
         editUserView.refresh(model.getModelData());
     }
+    public void onUserDelete(long id){
+        model.deleteUserById(id);
+        usersView.refresh(model.getModelData());
+    }
     public void setUsersView(UsersView usersView) {
         this.usersView = usersView;
     }
