d0a5f8ddd9e1cdf1d40952a16b0bd0228820dadd
Vladimir <decdom@mail.ru>
2018-03-22 21:26:16 +0300

    Update Human.java
    deleted field isSoldier, edited constructor, deleted fight() method

--- rush/projects/Refactoring/human/Human.java
+++ rush/projects/Refactoring/human/Human.java
@@ -12,8 +12,6 @@ public class Human {
 
     protected int[] size;
 
-    protected boolean isSoldier;
-
     public static final int FIRST = 1;
     public static final int SECOND = 2;
     public static final int THIRD = 3;
@@ -28,8 +26,9 @@ public class Human {
         return bloodGroup;
     }
 
-    public Human(boolean isSoldier) {
-        this.isSoldier = isSoldier;
+    public Human(String name, int age) {
+        this.name=name;
+        this.age=age;
         this.id = nextId;
         nextId++;
     }
@@ -54,12 +53,7 @@ public class Human {
         return course;
     }
 
-    public void live() {
-        if (isSoldier)
-            fight();
-    }
-
-    public void fight() {
+    public void live() {        
     }
 
     public int getId() {
