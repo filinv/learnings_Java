f2954d898dcdeea9f772d9b26a8ca49236245aee
Vladimir <decdom@mail.ru>
2018-03-22 21:03:11 +0300

    Create Work.java

---
+++ rush/projects/Refactoring/user/Work.java
@@ -0,0 +1,11 @@
+public class Work {
+    private String boss;
+
+    public String getBoss() {
+        return boss;
+    }
+
+    public void setBoss(String boss) {
+        this.boss = boss;
+    }
+}
