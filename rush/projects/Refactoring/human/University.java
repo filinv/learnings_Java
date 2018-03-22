7248f3b26482d6ce7aefae2ca78a7f0a195d3b25
Vladimir <decdom@mail.ru>
2018-03-22 21:01:57 +0300

    Create University.java

---
+++ rush/projects/Refactoring/human/University.java
@@ -0,0 +1,20 @@
+public class University extends Student {
+
+    public University(String name, int age) {
+        super(name, age, 0);
+    }
+
+    public Student getStudentWithAverageGrade() {
+        //TODO:
+        return null;
+    }
+
+    public Student getStudentWithMaxAverageGrade(double averageGrade) {
+        //TODO:
+        return null;
+    }
+
+    public void getStudentWithMinAverageGradeAndExpel() {
+        //TODO:
+    }
+}
