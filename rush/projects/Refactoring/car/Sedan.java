761a3125060414131cfd9a6948bf61a6e42987d9
Vladimir <decdom@mail.ru>
2018-03-24 22:03:40 +0300

    Update Sedan.java
    @Overrided public int getMaxSpeed().

--- rush/projects/Refactoring/car/Sedan.java
+++ rush/projects/Refactoring/car/Sedan.java
@@ -2,4 +2,9 @@ public class Sedan extends Car {
     public Sedan(int numberOfPassengers) {
         super(SEDAN, numberOfPassengers);
     }
+    @Override
+    public int getMaxSpeed() {
+        final int MAX_SEDAN_SPEED=120;
+        return MAX_SEDAN_SPEED;
+    }
 }
