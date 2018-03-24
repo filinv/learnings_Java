487e686d477d0a2562ca4cf34a84dfc674974553
Vladimir <decdom@mail.ru>
2018-03-24 20:47:53 +0300

    Update Human.java
    added fields protected Size size, private BloodGroup bloodGroup;getter and setter for BloodGroup and class Size;
    edited public void printSize().

--- rush/projects/Refactoring/human/Human.java
+++ rush/projects/Refactoring/human/Human.java
@@ -9,20 +9,16 @@ public class Human implements Alive{
     protected String name;
     private List<Human> children = new ArrayList<>();
 
-    protected int[] size;
+    protected Size size;
 
-    public static final int FIRST = 1;
-    public static final int SECOND = 2;
-    public static final int THIRD = 3;
-    public static final int FOURTH = 4;
-    private int bloodGroup;
+    private BloodGroup bloodGroup;
 
-    public void setBloodGroup(int code) {
-        bloodGroup = code;
+    public BloodGroup getBloodGroup() {
+        return bloodGroup;
     }
 
-    public int getBloodGroup() {
-        return bloodGroup;
+    public void setBloodGroup(BloodGroup bloodGroup) {
+        this.bloodGroup = bloodGroup;
     }
 
     public Human(String name, int age) {
@@ -68,10 +64,13 @@ public class Human implements Alive{
     }
 
     public void printSize() {
-        System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
+        System.out.println("Рост: " + size.height + " Вес: " + size.weight);
     }
     public String getPosition(){ return "Человек";}
     public void printData() {
         System.out.println(getPosition()+ ": " + name);
+    }
+    public class Size{
+        public int height, weight;
     }
 }
