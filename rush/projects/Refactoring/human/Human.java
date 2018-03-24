9a70a6233f58acbe90c32d030bce17c62bc9aa84
Vladimir <decdom@mail.ru>
2018-03-24 14:23:24 +0300

    Update Human.java
    added public String getPosition() and public void printData()

--- rush/projects/Refactoring/human/Human.java
+++ rush/projects/Refactoring/human/Human.java
@@ -73,4 +73,8 @@ public class Human implements Alive{
     public void printSize() {
         System.out.println("Рост: " + size[0] + " Вес: " + size[1]);
     }
+    public String getPosition(){ return "Человек";}
+    public void printData() {
+        System.out.println(getPosition()+ ": " + name);
+    }
 }
