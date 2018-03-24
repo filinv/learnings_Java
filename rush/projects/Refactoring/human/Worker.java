96b4539af7afc1d006151d24432c341cf67d7ba4
Vladimir <decdom@mail.ru>
2018-03-24 14:29:55 +0300

    Update Worker.java
    edited name of method setSlr to setSalary,
    edited constructor,
    deleted field private Human human,
    edited method's live() body
    added inheritance from Human

--- rush/projects/Refactoring/human/Worker.java
+++ rush/projects/Refactoring/human/Worker.java
@@ -1,21 +1,20 @@
-public class Worker {
-    private Human human;
+public class Worker extends Human{
     private double salary;
     public String company;
 
     public Worker(String name, int age) {
-        human = new Human(name, age);
+        super(name, age);
     }
 
     public void live() {
-        human.live();
+        super.live();
     }
 
     public double getSalary() {
         return salary;
     }
 
-    public void setSlr(double salary) {
+    public void setSalary(double salary) {
         this.salary = salary;
     }
 }
