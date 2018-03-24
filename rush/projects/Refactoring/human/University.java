ee33dd235d9e4cc788fbfdb56d62db6bdc072650
Vladimir <decdom@mail.ru>
2018-03-24 14:06:51 +0300

    Update University.java
    deleted inheritance;
    added fields private List<Student> students, private String name, private int age, getters and setters for new fields;
    refactored constructor;

--- rush/projects/Refactoring/human/University.java
+++ rush/projects/Refactoring/human/University.java
@@ -1,7 +1,29 @@
-public class University extends Student {
+public class University {
+    private List<Student> students = new ArrayList<>();
+    private String name;
+    private int age;
 
     public University(String name, int age) {
-        super(name, age, 0);
+        this.name=name;
+        this.age=age;
+    }
+    public List<Student> getStudents() {
+        return students;
+    }
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setAge(int age) {
+        this.age = age;
     }
 
     public Student getStudentWithAverageGrade() {
