f772524cc0de29a970dc6e290eeedeef5509c895
Vladimir <decdom@mail.ru>
2018-03-31 20:41:45 +0300

    Update ConsoleHelper.java
    added private static BufferedReader reader, public static void writeMessage(String message), public static String readString(), public static int readInt()

--- rush/projects/Chat/ConsoleHelper.java
+++ rush/projects/Chat/ConsoleHelper.java
@@ -1,2 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class ConsoleHelper {
+    private static BufferedReader reader=new BufferedReader(new InputStreamReader(System.in));
+    public static void writeMessage(String message){
+        System.out.println(message);
+    }
+    public static String readString(){
+        String line=null;
+        while (true) {
+            try {
+                line=reader.readLine();
+                break;
+            } catch (IOException e) {
+                System.out.println("Произошла ошибка при попытке ввода текста. Попробуйте еще раз.");
+                continue;
+            }
+        }
+        return line;
+    }
+    public static int readInt(){
+        int result=0;
+        while (true) {
+            try {
+                result=Integer.parseInt(readString());
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Произошла ошибка при попытке ввода числа. Попробуйте еще раз.");
+                continue;
+            }
+        }
+        return result;
+    }
 }
