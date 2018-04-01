d416f328b2ce300387cd5f7695cabbdfcb845cc7
Vladimir <decdom@mail.ru>
2018-04-01 19:05:22 +0300

    Update Client.java

--- rush/projects/Chat/client/Client.java
+++ rush/projects/Chat/client/Client.java
@@ -1,6 +1,37 @@
+import java.io.IOException;
+
 public class Client {
     protected Connection connection;
     private volatile boolean clientConnected=false;
-    
+
+    protected String getServerAddress(){
+        System.out.println("input address (ip/localhost):");
+        String address= ConsoleHelper.readString();
+        return address;
+    }
+    protected int getServerPort(){
+        System.out.println("input server port:");
+        int port=ConsoleHelper.readInt();
+        return port;
+    }
+    protected String getUserName(){
+        System.out.println("input your name:");
+        String name= ConsoleHelper.readString();
+        return name;
+    }
+    protected boolean shouldSendTextFromConsole(){
+        return true;
+    }
+    protected SocketThread getSocketThread(){
+        return new SocketThread();
+    }
+    protected void sendTextMessage(String text){
+        try {
+            connection.send(new Message(MessageType.TEXT,text));
+        } catch (IOException e) {
+            ConsoleHelper.writeMessage("your message is not sent...your disconnected");
+            clientConnected=false;
+        }
+    }
     public class SocketThread extends Thread{}
 }
