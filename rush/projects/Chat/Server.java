661613d03e05798f89b665be757bfabad8c615d7
Vladimir <decdom@mail.ru>
2018-04-01 12:57:54 +0300

    Update Server.java
    added import java.util.Map; import java.util.concurrent.ConcurrentHashMap; private static Map<String, Connection> connectionMap; public static void sendBroadcastMessage(Message message).

--- rush/projects/Chat/Server.java
+++ rush/projects/Chat/Server.java
@@ -1,8 +1,12 @@
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class Server {
+    private static Map<String, Connection> connectionMap=new ConcurrentHashMap<>();
+    
     public static void main(String[] args){
         int port=ConsoleHelper.readInt();
         try (ServerSocket serverSocket=new ServerSocket(port)){
@@ -17,6 +21,17 @@ public class Server {
             e.printStackTrace();
         }
     }
+    
+    public static void sendBroadcastMessage(Message message){
+        for(Map.Entry<String, Connection> pair: connectionMap.entrySet()){
+            try {
+                pair.getValue().send(message);
+            } catch (IOException e) {
+                System.out.println("message did not sent");
+            }
+        }
+    }
+    
     private static class Handler extends Thread{
         private Socket socket;
         public Handler(Socket socket){
