8386ac7fea1f098c64f94f5ab6cf92404e69f3c7
Vladimir <decdom@mail.ru>
2018-04-01 12:41:14 +0300

    Update Server.java
    added private static class Handler extends Thread; filled main method

--- rush/projects/Chat/Server.java
+++ rush/projects/Chat/Server.java
@@ -1,3 +1,26 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args){}
+    public static void main(String[] args){
+        int port=ConsoleHelper.readInt();
+        try (ServerSocket serverSocket=new ServerSocket(port)){
+            System.out.println("Server started");
+            while (true){
+                Socket socket=serverSocket.accept();//http://www.quizful.net/post/java-socket-programming
+                Handler handler=new Handler(socket);
+                handler.start();
+                continue;
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private static class Handler extends Thread{
+        private Socket socket;
+        public Handler(Socket socket){
+            this.socket=socket;
+        }
+    }
 }
