15531f49e900f0f0ee0d2b49d9c17b777b3a4419
Vladimir <decdom@mail.ru>
2018-04-01 10:17:20 +0300

    Update Message.java
    added implementation of Serializable, private final MessageType type; private final String data; getters of it, 2 constructors

--- rush/projects/Chat/Message.java
+++ rush/projects/Chat/Message.java
@@ -1,2 +1,23 @@
-public class Message {
+import java.io.Serializable;
+
+public class Message implements Serializable{
+    private final MessageType type;
+    private final String data;
+
+    public MessageType getType() {
+        return type;
+    }
+    public String getData() {
+        return data;
+    }
+
+    public Message(MessageType type) {
+        this.type = type;
+        this.data=null;
+    }
+
+    public Message(MessageType type, String data) {
+        this.type = type;
+        this.data = data;
+    }
 }
