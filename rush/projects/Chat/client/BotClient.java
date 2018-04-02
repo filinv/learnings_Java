3c5079d34a4c1ad47285f0ca1a798526aa90ceb5
Vladimir <decdom@mail.ru>
2018-04-02 21:53:35 +0300

    Update BotClient.java
    overrided protected void clientMainLoop() throws IOException, ClassNotFoundException; protected void processIncomingMessage(String message)

--- rush/projects/Chat/client/BotClient.java
+++ rush/projects/Chat/client/BotClient.java
@@ -20,6 +20,33 @@ public class BotClient extends Client {
     }
 
     public class BotSocketThread extends SocketThread{
+        @Override
+        protected void clientMainLoop() throws IOException, ClassNotFoundException {
+            sendTextMessage("Привет чатику. Я бот. Понимаю команды: дата, день, месяц, год, время, час, минуты, секунды.");
+            super.clientMainLoop();
+        }
 
+        @Override
+        protected void processIncomingMessage(String message) {
+            ConsoleHelper.writeMessage(message);
+            if(message!=null&&message.contains(": ")) {
+                String name = message.split(": ")[0];
+                String text = message.split(": ")[1];
+                Calendar calendar=Calendar.getInstance();
+                SimpleDateFormat dateFormat=null;
+                if(text.equals("дата"))dateFormat=new SimpleDateFormat("d.MM.YYYY");
+                else if(text.equals("день"))dateFormat=new SimpleDateFormat("d");
+                else if(text.equals("месяц"))dateFormat=new SimpleDateFormat("MMMM");
+                else if(text.equals("год"))dateFormat=new SimpleDateFormat("YYYY");
+                else if(text.equals("время"))dateFormat=new SimpleDateFormat("H:mm:ss");
+                else if(text.equals("час"))dateFormat=new SimpleDateFormat("H");
+                else if(text.equals("минуты"))dateFormat=new SimpleDateFormat("m");
+                else if(text.equals("секунды"))dateFormat=new SimpleDateFormat("s");
+                if(dateFormat!=null){
+                    String request="Информация для "+name+": "+dateFormat.format(calendar.getTime());
+                    sendTextMessage(request);
+                }
+            }
+        }
     }
 }
