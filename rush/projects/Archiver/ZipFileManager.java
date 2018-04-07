7158e9c76b51b8cd7cd00de874fca22c299cf7a8
Vladimir <decdom@mail.ru>
2018-04-07 21:02:49 +0300

    Update ZipFileManager.java
    write public void createZip(Path source) throws Exception; 
    imported java.io.IOException; java.io.InputStream; java.nio.file.Files; java.util.zip.ZipEntry; java.util.zip.ZipOutputStream;

--- rush/projects/Archiver/ZipFileManager.java
+++ rush/projects/Archiver/ZipFileManager.java
@@ -1,4 +1,9 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
 
 public class ZipFileManager {
     private Path zipFile;//full Path to file
@@ -6,5 +11,21 @@ public class ZipFileManager {
     public ZipFileManager(Path zipFile) {
         this.zipFile = zipFile;
     }
-    public void createZip(Path source) throws Exception{}
+    public void createZip(Path source) throws Exception{
+        try(ZipOutputStream zipOutputStream=new ZipOutputStream(Files.newOutputStream(zipFile)))
+        {
+            ZipEntry zipEntry=new ZipEntry(source.getFileName().toString());
+            zipOutputStream.putNextEntry(zipEntry);
+            try(InputStream inputStream=Files.newInputStream(source)){
+                while (inputStream.available()>0) {
+                    zipOutputStream.write(inputStream.read());
+                }
+            } catch (IOException e){
+                e.printStackTrace();
+            }
+            zipOutputStream.closeEntry();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 }
