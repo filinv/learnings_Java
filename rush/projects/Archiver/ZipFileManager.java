3e708f1bcd8c7fd5a50060c133b7deb0ef84008b
Vladimir <decdom@mail.ru>
2018-04-07 20:19:35 +0300

    Create ZipFileManager.java

---
+++ rush/projects/Archiver/ZipFileManager.java
@@ -0,0 +1,10 @@
+import java.nio.file.Path;
+
+public class ZipFileManager {
+    private Path zipFile;//full Path to file
+
+    public ZipFileManager(Path zipFile) {
+        this.zipFile = zipFile;
+    }
+    public void createZip(Path source) throws Exception{}
+}
