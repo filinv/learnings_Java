43b8a865a807aa9e7879e6295f7ec28c679c6fb8
Vladimir <decdom@mail.ru>
2018-04-09 14:44:40 +0300

    Update ZipCreateCommand.java
    write method execute()

--- rush/projects/Archiver/command/ZipCreateCommand.java
+++ rush/projects/Archiver/command/ZipCreateCommand.java
@@ -1,5 +1,18 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipCreateCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Создание архива.");
+            ZipFileManager zipFileManager=getZipFileManager();
+            ConsoleHelper.writeMessage("ввести полное имя файла или директории для архивации!");
+            Path source= Paths.get(ConsoleHelper.readString());
+            zipFileManager.createZip(source);
+            ConsoleHelper.writeMessage("Архив создан.");
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
