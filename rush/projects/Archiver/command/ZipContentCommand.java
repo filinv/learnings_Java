9213f9766d7afa282b93bc34f76d108703315965
Vladimir <decdom@mail.ru>
2018-04-09 19:41:33 +0300

    Update ZipContentCommand.java
    wrote method execute();

--- rush/projects/Archiver/command/ZipContentCommand.java
+++ rush/projects/Archiver/command/ZipContentCommand.java
@@ -1,5 +1,12 @@
 public class ZipContentCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Просмотр содержимого архива.");
+        ZipFileManager fileManager=getZipFileManager();
+        ConsoleHelper.writeMessage("Содержимое архива:");
+        for(FileProperties p:fileManager.getFilesList()){
+            ConsoleHelper.writeMessage(p.toString());
+        }
+        ConsoleHelper.writeMessage("Содержимое архива прочитано.");
     }
 }
