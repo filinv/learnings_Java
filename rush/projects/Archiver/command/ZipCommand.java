3927b9f8bb6e23a8b27dc6e00769c13fd53e9928
Vladimir <decdom@mail.ru>
2018-04-09 14:45:20 +0300

    Update ZipCommand.java

--- rush/projects/Archiver/command/ZipCommand.java
+++ rush/projects/Archiver/command/ZipCommand.java
@@ -1,2 +1,8 @@
+import java.nio.file.Paths;
+
 public abstract class ZipCommand implements Command{
+    public ZipFileManager getZipFileManager() throws Exception{
+        ConsoleHelper.writeMessage("ввести полный путь файла архива!");
+        return new ZipFileManager(Paths.get(ConsoleHelper.readString()));
+    }
 }
