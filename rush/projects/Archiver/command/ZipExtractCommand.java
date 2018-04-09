6962f3d1de32a06a52b056e1027564d271cf7e1e
Vladimir <decdom@mail.ru>
2018-04-09 20:19:03 +0300

    Update ZipExtractCommand.java
    wrote method execute() and added imports

--- rush/projects/Archiver/command/ZipExtractCommand.java
+++ rush/projects/Archiver/command/ZipExtractCommand.java
@@ -1,5 +1,22 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipExtractCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        try {
+            ConsoleHelper.writeMessage("Распаковка архива.");
+
+            ZipFileManager zipFileManager = getZipFileManager();
+
+            ConsoleHelper.writeMessage("Введите полное имя файла или директории для деархивации:");
+            Path sourcePath = Paths.get(ConsoleHelper.readString());
+            zipFileManager.extractAll(sourcePath);
+
+            ConsoleHelper.writeMessage("Архив распакован.");
+
+        } catch (PathIsNotFoundException e) {
+            ConsoleHelper.writeMessage("Вы неверно указали имя файла или директории.");
+        }
     }
 }
