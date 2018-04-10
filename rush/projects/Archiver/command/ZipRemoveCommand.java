ae87e42fc7a0cf5f51188be1c2c321422d3783e7
Vladimir <decdom@mail.ru>
2018-04-10 19:46:15 +0300

    Update ZipRemoveCommand.java

--- rush/projects/Archiver/command/ZipRemoveCommand.java
+++ rush/projects/Archiver/command/ZipRemoveCommand.java
@@ -1,5 +1,20 @@
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class ZipRemoveCommand extends ZipCommand {
     @Override
     public void execute() throws Exception {
+        ConsoleHelper.writeMessage("Удаление файлов из архива.");
+
+        ZipFileManager zipFileManager = getZipFileManager();
+
+        //ConsoleHelper.writeMessage("Введите полное имя файла архива:");
+        //Path zipFile = Paths.get(ConsoleHelper.readString());
+        ConsoleHelper.writeMessage("Введите имя файла, который надо удалить из архива:");
+        Path removeFile = Paths.get(ConsoleHelper.readString());
+
+        zipFileManager.removeFile(removeFile);
+
+        ConsoleHelper.writeMessage("Архив обновлён.");
     }
 }
