786bb4e35d250d82087fb3adc98778e0fd6579c4
Vladimir <decdom@mail.ru>
2018-04-11 19:59:38 +0300

    Create Zombie.java

---
+++ rush/Threading/other/PlantsVsZombies/Zombie.java
@@ -0,0 +1,10 @@
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class Zombie extends Character {
+    private final static AtomicInteger idSequence = new AtomicInteger();
+    private final int id = idSequence.incrementAndGet();
+
+    protected int getId() {
+        return id;
+    }
+}
