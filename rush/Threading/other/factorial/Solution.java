76fbd2f10e1c04751e13ea4dcc4312c66ee7ae5c
Vladimir <decdom@mail.ru>
2018-04-11 20:14:47 +0300

    Create Solution.java

---
+++ rush/Threading/other/factorial/Solution.java
@@ -0,0 +1,32 @@
+import java.util.concurrent.*;
+
+/* 
+FutureTask
+Класс FactorialCalculator предназначен для вычисления факториала числа переданного в его конструктор в качестве параметра.
+К сожалению, он реализован некорректно и тебе необходимо это исправить.
+Внимательно разбери метод main класса Solution.
+*/
+
+public class Solution {
+    private static final ExecutorService threadpool = Executors.newFixedThreadPool(4);
+    private static final int n = 16;
+
+    public static void main(String args[]) throws InterruptedException, ExecutionException {
+        FactorialCalculator task = new FactorialCalculator(n);
+
+        System.out.println("Submitting Task ...");
+        Future future = threadpool.submit(task);
+        System.out.println("Task was submitted successfully");
+
+        while (!future.isDone()) {
+            System.out.println("Task is not completed yet....");
+            Thread.sleep(1);
+        }
+
+        System.out.println("Task is completed, let's check the result");
+        long factorial = (long) future.get();
+
+        System.out.println("Factorial of " + n + " is : " + factorial);
+        threadpool.shutdown();
+    }
+}
