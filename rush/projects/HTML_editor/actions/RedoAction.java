998c5756f5b20385bcc60df310a1a2022a1fff9b
Vladimir <decdom@mail.ru>
2018-04-12 15:59:22 +0300

    Create RedoAction.java

---
+++ rush/projects/HTML_editor/actions/RedoAction.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+
+/**Класс возврата действия */
+public class RedoAction extends AbstractAction {
+    private View view;
+
+    public RedoAction(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+    }
+}
