3d95e9485770ff53528d77714e972a8987f79774
Vladimir <decdom@mail.ru>
2018-04-12 16:02:34 +0300

    Create TextEditMenuListener.java

---
+++ rush/projects/HTML_editor/listeners/TextEditMenuListener.java
@@ -0,0 +1,22 @@
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class TextEditMenuListener implements MenuListener {
+    private View view;
+
+    public TextEditMenuListener(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+    }
+}
