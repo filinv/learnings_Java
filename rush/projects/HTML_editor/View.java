7f91c2fbda00b9992e2ed5c57811e581be173b65
Vladimir <decdom@mail.ru>
2018-04-12 09:10:27 +0300

    Update View.java
    added fields: private JTabbedPane tabbedPane=new JTabbedPane(); private JTextPane htmlTextPane=new JTextPane();  private JEditorPane plainTextPane=new JEditorPane();

--- rush/projects/HTML_editor/View.java
+++ rush/projects/HTML_editor/View.java
@@ -4,6 +4,14 @@ import java.awt.event.ActionListener;
 
 public class View extends JFrame implements ActionListener {
     private Controller controller;
+    /**панель с двумя вкладками. */
+    private JTabbedPane tabbedPane=new JTabbedPane();
+
+    /**компонент для визуального редактирования html. Он будет размещен на первой вкладке.*/
+    private JTextPane htmlTextPane=new JTextPane();
+
+    /**компонент для редактирования html в виде текста, он будет отображать код html (теги и их содержимое). */
+    private JEditorPane plainTextPane=new JEditorPane();
     @Override
     public void actionPerformed(ActionEvent e) {
     }
