a2e87c9f942b2f62276e8f427fba8d107c2ee41f
Vladimir <decdom@mail.ru>
2018-04-12 16:02:01 +0300

    Create UndoMenuListener.java

---
+++ rush/projects/HTML_editor/listeners/UndoMenuListener.java
@@ -0,0 +1,27 @@
+import javax.swing.*;
+import javax.swing.event.MenuEvent;
+import javax.swing.event.MenuListener;
+
+public class UndoMenuListener implements MenuListener {
+    private View view;
+    private JMenuItem undoMenuItem;
+    private JMenuItem redoMenuItem;
+
+    public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
+        this.view = view;
+        this.undoMenuItem = undoMenuItem;
+        this.redoMenuItem = redoMenuItem;
+    }
+
+    @Override
+    public void menuSelected(MenuEvent e) {
+    }
+
+    @Override
+    public void menuDeselected(MenuEvent e) {
+    }
+
+    @Override
+    public void menuCanceled(MenuEvent e) {
+    }
+}
