cdfff8884d270f0ff06df75b8758ae17f47f6fb8
Vladimir <decdom@mail.ru>
2018-04-12 18:00:59 +0300

    Update Controller.java
    public void resetDocument();

--- rush/projects/HTML_editor/Controller.java
+++ rush/projects/HTML_editor/Controller.java
@@ -19,6 +19,14 @@ public class Controller {
     public HTMLDocument getDocument() {
         return document;
     }
+    public void resetDocument(){
+        if(document!=null){
+            document.removeUndoableEditListener(view.getUndoListener());
+        }
+        document=(HTMLDocument)new HTMLEditorKit().createDefaultDocument();
+        document.addUndoableEditListener(view.getUndoListener());
+        view.update();
+    }
     public void exit(){
         System.exit(0);
     }
