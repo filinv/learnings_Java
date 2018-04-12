f436ee7063ba7aa473518b9e43af609a0f91993d
Vladimir <decdom@mail.ru>
2018-04-12 19:25:01 +0300

    Create HTMLFileFilter.java

---
+++ rush/projects/HTML_editor/HTMLFileFilter.java
@@ -0,0 +1,17 @@
+import javax.swing.filechooser.FileFilter;
+import java.io.File;
+
+public class HTMLFileFilter extends FileFilter {
+    @Override
+    public boolean accept(File f) {
+        if (f.isDirectory()) return true;
+        else if (f.getName().toLowerCase().endsWith(".html") || f.getName().toLowerCase().endsWith(".htm"))
+                return true;
+        return false;
+    }
+
+    @Override
+    public String getDescription() {
+        return "HTML и HTM файлы";
+    }
+}
