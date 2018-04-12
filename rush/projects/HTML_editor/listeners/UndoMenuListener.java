7c4950ebd0610785294703733c14d40ce9d93def
Vladimir <decdom@mail.ru>
2018-04-12 16:37:19 +0300

    Update UndoMenuListener.java
    added comments, wrote menuSelected(MenuEvent e)

--- rush/projects/HTML_editor/listeners/UndoMenuListener.java
+++ rush/projects/HTML_editor/listeners/UndoMenuListener.java
@@ -2,9 +2,16 @@ import javax.swing.*;
 import javax.swing.event.MenuEvent;
 import javax.swing.event.MenuListener;
 
+/**Этот слушатель будет следить за меню, а если конкретнее, то за моментом,
+ * когда меню редактирования будет выбрано пользователем.
+ * В этот момент он будет запрашивать у представления можем ли мы сейчас
+ * отменить или вернуть какое-то действие, и в зависимости от этого делать доступными
+ * или не доступными пункты меню "Отменить" и "Вернуть". */
 public class UndoMenuListener implements MenuListener {
-    private View view;
+    private View view;    
+    /**Пункт меню "Отменить"*/
     private JMenuItem undoMenuItem;
+    /**Пункт меню "Вернуть"*/
     private JMenuItem redoMenuItem;
 
     public UndoMenuListener(View view, JMenuItem undoMenuItem, JMenuItem redoMenuItem) {
@@ -13,8 +20,13 @@ public class UndoMenuListener implements MenuListener {
         this.redoMenuItem = redoMenuItem;
     }
 
+    /**будет вызываться перед показом меню.*/
     @Override
     public void menuSelected(MenuEvent e) {
+        if(view.canUndo())undoMenuItem.setEnabled(true);
+        else undoMenuItem.setEnabled(false);
+        if(view.canRedo())redoMenuItem.setEnabled(true);
+        else redoMenuItem.setEnabled(false);
     }
 
     @Override
