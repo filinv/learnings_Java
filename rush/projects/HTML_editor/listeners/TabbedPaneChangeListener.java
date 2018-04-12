73cfaf58c9aea8a0a94bd3160398fd5a5ab8367d
Vladimir <decdom@mail.ru>
2018-04-12 09:34:15 +0300

    Create TabbedPaneChangeListener.java

---
+++ rush/projects/HTML_editor/listeners/TabbedPaneChangeListener.java
@@ -0,0 +1,16 @@
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+/**Этот класс будет слушать и обрабатывать изменения состояния панели вкладок. */
+public class TabbedPaneChangeListener implements ChangeListener {
+    private View view;
+
+    public TabbedPaneChangeListener(View view) {
+        this.view = view;
+    }
+
+    @Override
+    public void stateChanged(ChangeEvent e) {
+        view.selectedTabChanged();
+    }
+}
