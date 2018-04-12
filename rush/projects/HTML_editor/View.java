aa0ddaefee7b99adc0c08ab6692aa1dd576fed45
Vladimir <decdom@mail.ru>
2018-04-12 17:33:50 +0300

    Update View.java
    added public boolean isHtmlTabSelected();

--- rush/projects/HTML_editor/View.java
+++ rush/projects/HTML_editor/View.java
@@ -105,6 +105,12 @@ public class View extends JFrame implements ActionListener {
     public void resetUndo(){
         undoManager.discardAllEdits();
     }
+    /**Пункты меню, отвечающие за стиль, шрифт, цвет и т.д.
+     * должны быть доступны только тогда,
+     * когда в нашем редакторе выбрана первая вкладка */
+    public boolean isHtmlTabSelected(){
+        return tabbedPane.getSelectedIndex()==0;
+    }
     public void exit(){
         controller.exit();
     }
