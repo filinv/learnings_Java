9f900b52ef0707afaf0049bb25f242ec3ff99269
Vladimir <decdom@mail.ru>
2018-04-12 17:17:06 +0300

    Update SubscriptAction.java

--- rush/projects/HTML_editor/actions/SubscriptAction.java
+++ rush/projects/HTML_editor/actions/SubscriptAction.java
@@ -1,19 +1,25 @@
+import javax.swing.*;
+import javax.swing.text.MutableAttributeSet;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import javax.swing.text.StyledEditorKit;
 import java.awt.event.ActionEvent;
 
 /**отвечает за стиль текста "Подстрочный знак" */
 public class SubscriptAction extends StyledEditorKit.StyledTextAction {
-    /**
-     * Creates a new StyledTextAction from a string action name.
-     *
-     * @param nm the name of the action
-     */
+
     public SubscriptAction() {
-        super("Подстрочный знак");
+        super(StyleConstants.Subscript.toString());
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
-
+        JEditorPane editor = getEditor(e);
+        if (editor != null) {
+            MutableAttributeSet mutableAttributeSet = getStyledEditorKit(editor).getInputAttributes();
+            SimpleAttributeSet simpleAttributeSet = new SimpleAttributeSet();
+            StyleConstants.setSubscript(simpleAttributeSet, !StyleConstants.isSubscript(mutableAttributeSet));
+            setCharacterAttributes(editor, simpleAttributeSet, false);
+        }
     }
 }
