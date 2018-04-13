90bdf9397d320dfbd96f0f6392089d07abc0762b
Vladimir <decdom@mail.ru>
2018-04-13 17:37:12 +0300

    Create SmsLogger.java

---
+++ rush/Patterns/chainOfResponsibility/SmsLogger.java
@@ -0,0 +1,11 @@
+public class SmsLogger extends AbstractLogger {
+
+    public SmsLogger(int level) {
+        super(level);
+    }
+
+    @Override
+    public void info(String message) {
+        System.out.println("Send sms to CEO: " + message);
+    }
+}
