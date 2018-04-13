e55583aa2788164b21cef867b9c742dc9ac35adc
Vladimir <decdom@mail.ru>
2018-04-13 18:50:07 +0300

    Create Tablet.java

---
+++ rush/projects/restaurant/Tablet.java
@@ -0,0 +1,13 @@
+public class Tablet {
+    /**это номер планшета,
+     * чтобы можно было однозначно установить,
+     * откуда поступил заказ */
+    final int number;
+
+    public Tablet(int number) {
+        this.number = number;
+    }
+    /**будет создавать заказ из тех блюд,
+     * которые выберет пользователь */
+    public void createOrder(){}
+}
