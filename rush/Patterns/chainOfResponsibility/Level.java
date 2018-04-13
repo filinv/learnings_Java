db6e50e1dc02e500aa7e4d2292507c53f95ec650
Vladimir <decdom@mail.ru>
2018-04-13 17:39:15 +0300

    Create Level.java

---
+++ rush/Patterns/chainOfResponsibility/Level.java
@@ -0,0 +1,6 @@
+public class Level {
+    public static final int INFO = 100;
+    public static final int WARN = 200;
+    public static final int ERROR = 300;
+    public static final int FATAL = 400;
+}
