485f36cc993ba700712cc5a5335d04bd796a5dec
Vladimir <decdom@mail.ru>
2018-04-13 17:37:59 +0300

    Create ConsoleLogger.java

---
+++ rush/Patterns/chainOfResponsibility/ConsoleLogger.java
@@ -0,0 +1,11 @@
+public class ConsoleLogger extends AbstractLogger {
+
+    public ConsoleLogger(int level) {
+        super(level);
+    }
+
+    @Override
+    public void info(String message) {
+        System.out.println("Logging to console: " + message);
+    }
+}
