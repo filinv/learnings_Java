6dad3b24cf9ce411e55515dbfa3bcce685974005
Vladimir <decdom@mail.ru>
2018-04-13 18:50:29 +0300

    Create Order.java

---
+++ rush/projects/restaurant/Order.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+public class Order {
+    private final Tablet tablet;
+    protected List<Dish> dishes;
+
+    public Order(Tablet tablet) {
+        this.tablet = tablet;
+    }
+}
