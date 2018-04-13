5042b5c53f4ec3d7641f6ca9b8f6d3a21b0e30b8
Vladimir <decdom@mail.ru>
2018-04-13 17:37:36 +0300

    Create AbstractLogger.java

---
+++ rush/Patterns/chainOfResponsibility/AbstractLogger.java
@@ -0,0 +1,23 @@
+public abstract class AbstractLogger implements Logger {
+    int level;
+    Logger next;
+
+    public AbstractLogger(int level) {
+        this.level = level;
+    }
+
+    @Override
+    public void inform(String message, int level) {
+        if (this.level <= level) {
+            info(message);
+        }
+        if (next != null) {
+            next.inform(message, level);
+        }
+    }
+
+    @Override
+    public void setNext(Logger next) {
+        this.next = next;
+    }
+}
