02c81cc2eccd5265f2372095d518c2421c73d692
Vladimir <decdom@mail.ru>
2018-04-14 10:55:30 +0300

    Create Waiter.java

---
+++ rush/projects/restaurant/kitchen/Waiter.java
@@ -0,0 +1,9 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class Waiter implements Observer {
+    @Override
+    public void update(Observable cook, Object order) {
+        ConsoleHelper.writeMessage(order + " was cooked by " + cook);
+    }
+}
