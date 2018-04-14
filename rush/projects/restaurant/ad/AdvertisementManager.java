0c8f1da425d2bb5bf2d4db54c3317eb5e7f925c4
Vladimir <decdom@mail.ru>
2018-04-14 19:39:16 +0300

    Update AdvertisementManager.java

--- rush/projects/restaurant/ad/AdvertisementManager.java
+++ rush/projects/restaurant/ad/AdvertisementManager.java
@@ -1,3 +1,12 @@
 public class AdvertisementManager {
     private final AdvertisementStorage storage=AdvertisementStorage.getInstance();
+    private int timeSeconds;
+
+    public AdvertisementManager(int timeSeconds) {
+        this.timeSeconds = timeSeconds;
+    }
+    /**AdvertisementManager выполняет только одно единственное действие - обрабатывает рекламное видео */
+    public void processVideos(){
+        ConsoleHelper.writeMessage("calling processVideos method");
+    }
 }
