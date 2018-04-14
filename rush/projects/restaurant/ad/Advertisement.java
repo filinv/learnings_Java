82e4cbc5fe7470eae4ca2c56b07e0008d7182322
Vladimir <decdom@mail.ru>
2018-04-14 19:40:12 +0300

    Update Advertisement.java

--- rush/projects/restaurant/ad/Advertisement.java
+++ rush/projects/restaurant/ad/Advertisement.java
@@ -4,6 +4,9 @@ public class Advertisement {
     private long initialAmount;// - начальная сумма, стоимость рекламы в копейках.
     private int hits;// - количество оплаченных показов
     private int duration;// - продолжительность в секундах
+    /**Оно должно равняться стоимости одного показа
+     * рекламного объявления в копейках (initialAmount/hits) */
+    private long amountPerOneDisplaying;
 
     public Advertisement(Object content, String name, long initialAmount, int hits, int duration) {
         this.content = content;
@@ -11,5 +14,15 @@ public class Advertisement {
         this.initialAmount = initialAmount;
         this.hits = hits;
         this.duration = duration;
+        amountPerOneDisplaying=initialAmount/hits;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getDuration() {
+        return duration;
+    }
+    public long getAmountPerOneDisplaying() {
+        return amountPerOneDisplaying;
     }
 }
