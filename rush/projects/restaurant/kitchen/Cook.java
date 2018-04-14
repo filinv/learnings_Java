e5e5f3e7163bbded2689ab87dd7a4a2e16c710d0
Vladimir <decdom@mail.ru>
2018-04-14 10:42:45 +0300

    Create Cook.java

---
+++ rush/projects/restaurant/kitchen/Cook.java
@@ -0,0 +1,22 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class Cook extends Observable implements Observer{
+    private String name;
+
+    public Cook(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        /** observable - объект, который отправил нам значение
+         * arg - само значение, в нашем случае - это объект Order */
+        ConsoleHelper.writeMessage("Start cooking - " + arg);
+    }
+}
