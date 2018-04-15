d01ba8c32fbbd09e020b56d562c525f1fedcc757
Vladimir <decdom@mail.ru>
2018-04-15 10:36:58 +0300

    Update AdvertisementManager.java

--- rush/projects/restaurant/ad/AdvertisementManager.java
+++ rush/projects/restaurant/ad/AdvertisementManager.java
@@ -1,3 +1,8 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class AdvertisementManager {
     private final AdvertisementStorage storage=AdvertisementStorage.getInstance();
     private int timeSeconds;
@@ -6,7 +11,20 @@ public class AdvertisementManager {
         this.timeSeconds = timeSeconds;
     }
     /**AdvertisementManager выполняет только одно единственное действие - обрабатывает рекламное видео */
-    public void processVideos(){
-        ConsoleHelper.writeMessage("calling processVideos method");
+    public void processVideos() throws NoVideoAvailableException{
+        List<Advertisement> ads=storage.list();
+        if(ads.isEmpty())throw new NoVideoAvailableException();
+        
+        List<Advertisement> videoToBeShown = new ArrayList<>(ads);
+
+        Collections.sort(videoToBeShown, new Comparator<Advertisement>() {
+            @Override
+            public int compare(Advertisement o1, Advertisement o2) {
+                int result = Long.compare(o1.getAmountPerOneDisplaying(), o2.getAmountPerOneDisplaying());
+                if (result == 0)
+                    result = Long.compare(o1.getAmountPerOneDisplaying() / o1.getDuration(), o2.getAmountPerOneDisplaying() / o2.getDuration());
+                return result;
+            }
+        });        
     }
 }
