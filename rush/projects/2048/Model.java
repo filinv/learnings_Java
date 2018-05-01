feb0184a6ea7fb81614a39270a0686ae4e9f1d32
Vladimir <decdom@mail.ru>
2018-05-01 10:47:42 +0300

    Update Model.java

--- rush/projects/2048/Model.java
+++ rush/projects/2048/Model.java
@@ -1,3 +1,16 @@
 /** будет содержать игровую логику и хранить игровое поле.*/
 public class Model {
+  /** определяющая ширину игрового поля*/
+    private static final int FIELD_WIDTH = 4;
+
+    private Tile[][] gameTiles;
+
+    public Model() {
+        gameTiles=new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        for(int i=0;i<FIELD_WIDTH;i++){
+            for (int j=0;j<FIELD_WIDTH;j++){
+                gameTiles[i][j]=new Tile();
+            }
+        }
+    }
 }
