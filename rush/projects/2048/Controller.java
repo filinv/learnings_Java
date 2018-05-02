8f97f9d51b761de9cd8f6951373bb78ef19776e4
Vladimir <decdom@mail.ru>
2018-05-02 20:01:55 +0300

    Update Controller.java

--- rush/projects/2048/Controller.java
+++ rush/projects/2048/Controller.java
@@ -1,3 +1,11 @@
 /** будет следить за нажатием клавиш во время игры*/
-public class Controller {
+public class Controller extends KeyAdapter{
+    private Model model;
+    private View view;
+    public Tile[][] getGameTiles(){
+        return model.getGameTiles();
+    }
+    public int getScore(){
+        return model.score;
+    }
 }
