9801c7e69f0782a0706565908721a9febd35929d
Vladimir <decdom@mail.ru>
2018-05-02 20:50:21 +0300

    Update Main.java

--- rush/projects/2048/Main.java
+++ rush/projects/2048/Main.java
@@ -1,4 +1,17 @@
 /** будет содержать только метод main и служить точкой входа в наше приложение.*/
 public class Main {
-    public static void main(String[]args){}
+    public static void main(String[]args){
+        Model model=new Model();
+        Controller controller=new Controller(model);
+        JFrame game=new JFrame();
+        game.setTitle("2048");
+        game.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        game.setSize(450, 500);
+        game.setResizable(false);
+
+        game.add(controller.getView());
+
+        game.setLocationRelativeTo(null);
+        game.setVisible(true);
+    }
 }
