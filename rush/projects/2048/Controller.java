ec7c14617aea1b5505afc8f77806903932ccb9c3
Vladimir <decdom@mail.ru>
2018-05-02 20:39:55 +0300

    Update Controller.java

--- rush/projects/2048/Controller.java
+++ rush/projects/2048/Controller.java
@@ -1,11 +1,43 @@
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
+
 /** будет следить за нажатием клавиш во время игры*/
 public class Controller extends KeyAdapter{
     private Model model;
     private View view;
+    /** будет определять вес плитки при достижении которого игра будет считаться выигранной*/
+    private static final int WINNING_TILE = 2048;
+
+    public Controller(Model model) {
+        this.model = model;
+        view=new View(this);
+    }
+    public void resetGame(){
+        model.score=0;
+        view.isGameWon=false;
+        view.isGameLost=false;
+        model.resetGameTiles();
+    }
     public Tile[][] getGameTiles(){
         return model.getGameTiles();
     }
     public int getScore(){
         return model.score;
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        if(e.getKeyCode()==KeyEvent.VK_ESCAPE)resetGame();
+        if(model.canMove()==false)view.isGameLost=true;
+        if(view.isGameLost==false&&view.isGameWon==false){
+            switch (e.getKeyCode()){
+                case KeyEvent.VK_LEFT: model.left();break;
+                case KeyEvent.VK_RIGHT: model.right();break;
+                case KeyEvent.VK_UP: model.up();break;
+                case KeyEvent.VK_DOWN: model.down();break;
+            }
+            if(model.maxTile==WINNING_TILE)view.isGameWon=true;
+        }
+        view.repaint();
     }
 }
