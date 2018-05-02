dc548bcf8b7746bfd067b73be63297654b4abe9a
Vladimir <decdom@mail.ru>
2018-05-02 21:45:19 +0300

    Update Model.java
    added public void randomMove()

--- rush/projects/2048/Model.java
+++ rush/projects/2048/Model.java
@@ -179,4 +179,13 @@ public class Model {
             score=(int)previousScores.pop();
         }
     }
+    public void randomMove(){
+        int n = ((int) (Math.random() * 100)) % 4;
+        switch (n){
+            case 0: left();break;
+            case 1: right();break;
+            case 2: up();break;
+            case 3: down();break;
+        }
+    }
 }
