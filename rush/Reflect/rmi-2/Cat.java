a659d673ccbaa3e7a23debd4689311b85ba7f138
Vladimir <decdom@mail.ru>
2018-05-06 19:23:41 +0300

    Create Cat.java

---
+++ rush/Reflect/rmi-2/Cat.java
@@ -0,0 +1,20 @@
+import java.rmi.RemoteException;
+
+public class Cat implements Animal {
+
+    private String name;
+
+    public Cat(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void say() throws RemoteException {
+        System.out.println("meow");
+    }
+
+    @Override
+    public void printName() throws RemoteException {
+        System.out.print("Cat " + name + " ");
+    }
+}
