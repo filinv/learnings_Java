16629219cc13de22306a9c3697f6ab3d24f66ef4
Vladimir <decdom@mail.ru>
2018-05-06 10:39:40 +0300

    Create SomeInterfaceWithMethodsImpl.java

---
+++ rush/Reflect/proxy/SomeInterfaceWithMethodsImpl.java
@@ -0,0 +1,15 @@
+public class SomeInterfaceWithMethodsImpl implements SomeInterfaceWithMethods {
+    public void voidMethodWithoutArgs() {
+        System.out.println("inside voidMethodWithoutArgs");
+    }
+
+    public String stringMethodWithoutArgs() {
+        System.out.println("inside stringMethodWithoutArgs");
+        return null;
+    }
+
+    public void voidMethodWithIntArg(int i) {
+        System.out.println("inside voidMethodWithIntArg");
+        voidMethodWithoutArgs();
+    }
+}
