ef44f9398bef4b18c6d8d6f6054fb19d4de74701
Vladimir <decdom@mail.ru>
2018-05-06 19:24:01 +0300

    Create Dog.java

---
+++ rush/Reflect/rmi-2/Dog.java
@@ -0,0 +1,20 @@
+import java.rmi.RemoteException;
+
+public class Dog implements Animal {
+
+    private String name;
+
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void say() throws RemoteException {
+        System.out.println("woof");
+    }
+
+    @Override
+    public void printName() throws RemoteException {
+        System.out.print("Dog " + name + " ");
+    }
+}
