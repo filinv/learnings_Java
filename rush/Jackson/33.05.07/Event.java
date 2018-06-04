ad3797f813bc95d63daf90eb666b27f09143f8d2
Vladimir <decdom@mail.ru>
2018-06-04 20:50:21 +0300

    Create Event.java

---
+++ rush/Jackson/33.05.07/Event.java
@@ -0,0 +1,14 @@
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.util.Date;
+
+public class Event {
+    public String name;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "dd-MM-yyyy hh:mm:ss")
+    public Date eventDate;
+
+    public Event(String name) {
+        this.name = name;
+        eventDate = new Date();
+    }
+}
