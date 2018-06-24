75dd4748ebe8bc288ad181b5c91ed350e0182b3b
Vladimir <decdom@mail.ru>
2018-06-24 21:35:11 +0300

    Update Tetris.java
    added getters for private fields

--- rush/projects/tetris/Tetris.java
+++ rush/projects/tetris/Tetris.java
@@ -2,4 +2,10 @@ public class Tetris {
     private Field field;
     private Figure figure;
     public static void main(String [] args){}
+    public Field getField() {
+        return field;
+    }
+    public Figure getFigure() {
+        return figure;
+    }
 }
