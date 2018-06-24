b01d7cd719cb605cf5be73f8af0620b887f9db03
Vladimir <decdom@mail.ru>
2018-06-24 22:17:21 +0300

    Update FigureFactory.java

--- rush/projects/tetris/FigureFactory.java
+++ rush/projects/tetris/FigureFactory.java
@@ -1,4 +1,37 @@
 /** С его помощью будем создавать фигуры различных форм*/
 public class FigureFactory {
-    public static Figure createRandomFigure(int x,int y){return null;}
+    /** Набор из шести шаблонов для фигурок */
+    public static final int[][][] BRICKS = {{
+            {1, 1, 0},                          //   X X
+            {0, 1, 1},                          //     X X
+            {0, 0, 0}}, {                       //
+
+            {1, 0, 0},                          //   X
+            {1, 1, 0},                          //   X X
+            {0, 1, 0}}, {                       //     X
+
+            {0, 1, 0},                          //   X
+            {0, 1, 0},                          //   X
+            {0, 1, 0}}, {                       //   X
+
+            {1, 1, 0},                          //   X X
+            {1, 1, 0},                          //   X X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {0, 1, 0},                          //     X
+            {0, 0, 0}}, {                       //
+
+            {1, 1, 1},                          //   X X X
+            {1, 1, 1},                          //   X X X
+            {0, 0, 0}}                          //
+    };
+
+    /**
+     * Метод выбирает случайный шаблон и создает с ним новую фигурку.
+     */
+    public static Figure createRandomFigure(int x, int y) {
+        int index = (int) (Math.random() * 6);
+        return new Figure(x, y, BRICKS[index]);
+    }
 }
