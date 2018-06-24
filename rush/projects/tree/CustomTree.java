be363da6ff0ec51fe660adc8bf4db8c7b198aa31
Vladimir <decdom@mail.ru>
2018-06-24 21:23:19 +0300

    Update CustomTree.java
    added: public Entry<String> getElement(String elementName), public boolean remove(Object o), public List<Entry> getChildsAndParent(Entry parent), two fields in class Entry isLeft and isRight

--- rush/projects/tree/CustomTree.java
+++ rush/projects/tree/CustomTree.java
@@ -31,6 +31,22 @@ public class CustomTree extends AbstractList<String> implements Cloneable,Serial
         //throw new UnsupportedOperationException();
         return null;
     }
+    /** метод ввёл для простоты тестирования:
+     *  ищет элемент по имени и возвращает его*/
+    public Entry<String> getElement(String elementName){
+        if(elementName==null)return null;
+        Queue<Entry<String>>queue=new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            Entry<String>rootElement=queue.poll();
+            if(rootElement.elementName.equals(elementName))return rootElement;
+            else {
+                if(rootElement.leftChild!=null)queue.offer(rootElement.leftChild);
+                if(rootElement.rightChild!=null)queue.offer(rootElement.rightChild);
+            }
+        }
+        return null;
+    }
     /** возвращает текущее количество элементов в дереве
      * После добавления N элементов в дерево с помощью метода add,
      * метод size должен возвращать N*/
@@ -57,28 +73,92 @@ public class CustomTree extends AbstractList<String> implements Cloneable,Serial
                 if(rootElement.isAvailableToAddChildren()){
                     if(rootElement.availableToAddLeftChildren){
                         rootElement.leftChild=new Entry<>(s);
+                        rootElement.leftChild.isLeft=true;
                         rootElement.leftChild.parent=rootElement;
                         count++;
-                        //System.out.println("Added left element named "+rootElement.leftChild.elementName+" with rootElement "+rootElement.leftChild.parent.elementName);
+                        //System.out.println("Added left element named "+rootElement.leftChild.elementName+" with parent "+rootElement.leftChild.parent.elementName);
                     } else if(rootElement.availableToAddRightChildren){
                         rootElement.rightChild=new Entry<>(s);
+                        rootElement.rightChild.isRight=true;
                         rootElement.rightChild.parent=rootElement;
                         count++;
-                        //System.out.println("Added right element named "+rootElement.rightChild.elementName+" with rootElement "+rootElement.rightChild.parent.elementName);
+                        //System.out.println("Added right element named "+rootElement.rightChild.elementName+" with parent "+rootElement.rightChild.parent.elementName);
                     }
                     return true;
                 } else {
-                    queue.offer(rootElement.leftChild);
-                    queue.offer(rootElement.rightChild);
+                    if(rootElement.leftChild!=null)queue.offer(rootElement.leftChild);
+                    else {
+                        rootElement.availableToAddLeftChildren=true;
+                        queue.offer(rootElement);
+                    }
+                    if(rootElement.rightChild!=null)queue.offer(rootElement.rightChild);
+                    else {
+                        rootElement.availableToAddRightChildren=true;
+                        queue.offer(rootElement);
+                    }
                 }
             }
         }
         return false;
     }
+    /** будет удалять элемент дерева имя которого было полученного в качестве параметра.
+     * Если переданный объект не является строкой,
+     * метод должен бросить UnsupportedOperationException*/
     @Override
-    public String remove(int index) {
-        throw new UnsupportedOperationException();
-        //return (String)super.remove(index);
+    public boolean remove(Object o) {
+        if(o instanceof String){
+            if(o==null)return false;
+            List<Entry>branch=null;
+            String elementName=(String)o;
+            if(root.elementName.equals(elementName)){
+                root=new Entry<>("0");
+                return true;
+            }
+            Queue<Entry<String>>queue=new LinkedList<>();
+            queue.offer(root);
+            while (!queue.isEmpty()){
+                Entry<String>rootElement=queue.poll();
+                if(rootElement.elementName.equals(elementName)){
+                    //System.out.println("element was founded");
+                    branch=getChildsAndParent(rootElement);
+                    if(rootElement.isLeft){
+                        rootElement.parent.availableToAddLeftChildren=true;
+                        //System.out.println("may add left children? "+rootElement.parent.availableToAddLeftChildren);
+                    }
+                    else{
+                        rootElement.parent.availableToAddRightChildren=true;
+                        //System.out.println("may add right children? "+rootElement.parent.availableToAddRightChildren);
+                    }
+                    break;
+                } else {
+                    if(rootElement.leftChild!=null)queue.offer(rootElement.leftChild);
+                    if(rootElement.rightChild!=null)queue.offer(rootElement.rightChild);
+                }
+            }
+            for(Entry<String> element:branch){
+                if(element.leftChild!=null)element.leftChild=null;
+                if(element.rightChild!=null)element.rightChild=null;
+                if(element.isLeft)element.parent.leftChild=null;
+                else if(element.isRight)element.parent.rightChild=null;
+                count--;
+            }
+            //System.out.println(branch.get(0).elementName);
+            return true;
+        } else throw new UnsupportedOperationException();
+    }
+    /** позволяет выбрать целую ветвь*/
+    public List<Entry> getChildsAndParent(Entry parent){
+        if(parent==null)return null;
+        List<Entry> family=new ArrayList<>();
+        Queue<Entry> queue=new LinkedList<>();
+        queue.offer(parent);
+        while (!queue.isEmpty()){
+            Entry rootElement=queue.poll();
+            if(rootElement.leftChild!=null)queue.offer(rootElement.leftChild);
+            if(rootElement.rightChild!=null)queue.offer(rootElement.rightChild);
+            family.add(rootElement);
+        }
+        return family;
     }
     @Override
     public List subList(int fromIndex, int toIndex) {
@@ -122,6 +202,7 @@ public class CustomTree extends AbstractList<String> implements Cloneable,Serial
         String elementName;
         int lineNumber;
         boolean availableToAddLeftChildren, availableToAddRightChildren;
+        boolean isLeft=false, isRight=false;
         Entry<T> parent, leftChild, rightChild;
         public Entry(String string){
             elementName=string;
