f87c762dd05d58d7b3c3bb86516ddba1b804ee76
Vladimir <decdom@mail.ru>
2018-06-24 10:27:12 +0300

    Update CustomTree.java
    added public String getParent(String s), public boolean add(String s), public int size().

--- rush/projects/tree/CustomTree.java
+++ rush/projects/tree/CustomTree.java
@@ -1,42 +1,79 @@
 import java.io.Serializable;
-import java.util.AbstractList;
-import java.util.Collection;
-import java.util.List;
+import java.util.*;
 
 /* 
 Построй дерево(1)
 */
-public class CustomTree extends AbstractList implements Cloneable,Serializable{
+public class CustomTree extends AbstractList<String> implements Cloneable,Serializable{
     private int index;
     private Collection<? extends String> c;
     Entry<String> root;
+    private int count=0;
+
+    public CustomTree() {
+        this.root = new Entry<>("0");
+    }
 
     public static void main(String[] args) {
         List<String> list = new CustomTree();
-        for (int i = 1; i < 16; i++) {
+        for (int i = 1; i < 131; i++) {
             list.add(String.valueOf(i));
         }
-        //System.out.println("Expected 3, actual is " + ((CustomTree) list).getParent("8"));
-        list.remove("5");
+        System.out.println("Expected parent for 129 is 64, actual is " + ((CustomTree) list).getParent("129"));
+        System.out.println("Expected parent for 5 is 2, actual is " + ((CustomTree) list).getParent("5"));
+        System.out.println(list.size());
+        //list.remove("5");
         //System.out.println("Expected null, actual is " + ((CustomTree) list).getParent("11"));
     }
 
     @Override
     public String get(int index) {
-        throw new UnsupportedOperationException();
-        //return null;
+        //throw new UnsupportedOperationException();
+        return null;
     }
+    /** возвращает текущее количество элементов в дереве
+     * После добавления N элементов в дерево с помощью метода add,
+     * метод size должен возвращать N*/
     @Override
     public int size() {
-        return 0;
+        return count;
     }
     public String set(int index, String element) {
         throw new UnsupportedOperationException();
         //return (String)super.set(index, element);
     }
-    public void add(int index, String element) {
-        throw new UnsupportedOperationException();
-        //super.add(index, element);
+
+    /** добавляет элементы дерева,
+     * в качестве параметра принимает имя элемента (elementName),
+     * искать место для вставки начинаем слева направо */
+    public boolean add(String s){
+        if(s==null)return false;
+        Queue<Entry<String>> queue=new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            Entry<String>rootElement=queue.poll();
+            if(rootElement!=null){
+                rootElement.checkChildren();
+                if(rootElement.isAvailableToAddChildren()){
+                    if(rootElement.availableToAddLeftChildren){
+                        rootElement.leftChild=new Entry<>(s);
+                        rootElement.leftChild.parent=rootElement;
+                        count++;
+                        //System.out.println("Added left element named "+rootElement.leftChild.elementName+" with rootElement "+rootElement.leftChild.parent.elementName);
+                    } else if(rootElement.availableToAddRightChildren){
+                        rootElement.rightChild=new Entry<>(s);
+                        rootElement.rightChild.parent=rootElement;
+                        count++;
+                        //System.out.println("Added right element named "+rootElement.rightChild.elementName+" with rootElement "+rootElement.rightChild.parent.elementName);
+                    }
+                    return true;
+                } else {
+                    queue.offer(rootElement.leftChild);
+                    queue.offer(rootElement.rightChild);
+                }
+            }
+        }
+        return false;
     }
     @Override
     public String remove(int index) {
@@ -57,6 +94,30 @@ public class CustomTree extends AbstractList implements Cloneable,Serializable{
         throw new UnsupportedOperationException();
         //return super.addAll(index, c);
     }
+    /** возвращает имя родителя элемента дерева,
+     * имя которого было полученного в качестве параметра */
+    public String getParent(String s){
+        if(root==null||s==null)return null;
+        Queue<Entry<String>>queue=new LinkedList<>();
+        queue.offer(root);
+        while (!queue.isEmpty()){
+            Entry<String>rootElement=queue.poll();
+            //System.out.println("now checked element name is "+rootElement.elementName);
+            if(rootElement.elementName.equals(s)){
+                if(rootElement.parent==null){
+                    return "Have no parent";
+                } else return rootElement.parent.elementName;
+            } else {
+                if(rootElement.leftChild!=null){
+                    queue.offer(rootElement.leftChild);
+                }
+                if(rootElement.rightChild!=null){
+                    queue.offer(rootElement.rightChild);
+                }
+            }
+        }
+        return null;}
+
     static class Entry<T> implements Serializable{
         String elementName;
         int lineNumber;
