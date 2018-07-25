bf41f0a4b06833f4e03d673c406f6effb2f8109b
Vladimir <decdom@mail.ru>
2018-07-25 11:56:50 +0300

    Create SpeedometerView.java

---
+++ rush/inProgress/withoutTrying/3609/SpeedometerView.java
@@ -0,0 +1,5 @@
+public class SpeedometerView {
+    public void printCarDetails(String brand, String model, int speed) {
+        System.out.println("Car: \nBrand: " + brand + "\nModel: " + model + "\nCurrent Speed: " + speed + "\n");
+    }
+}
